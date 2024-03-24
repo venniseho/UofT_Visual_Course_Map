2413eb4d5cf1271a8f3115bc43701b04c4d7dae3
RF216 <157068144+RF216@users.noreply.github.com>
2024-03-24 14:24:55 -0400

    Finish Prereq + Exclusions

--- graph_course.py
+++ graph_course.py
@@ -9,11 +9,24 @@ Creators:
 - Vennise Ho
 """
 from __future__ import annotations
+from typing import Any
 
 
-class _Course:
+class Expr:
+    """A general expression in our abstract syntax tree
     """
-    A course in a graph (acts as a vertex).
+
+    def __init__(self) -> None:
+        pass
+
+    def evaluate(self) -> Any:
+        """Evaluates an expression, whetehr it;s a course or a BoolOp
+        """
+        raise NotImplementedError
+
+
+class _Course(Expr):
+    """A course in a graph (acts as a vertex).
 
     Instance Attributes:
     - code: the course code (ex. CSC111)
@@ -23,7 +36,7 @@ class _Course:
     Representation Invariants:
     """
     code: str
-    prerequisites: list[set]
+    prerequisites: BoolOp
     dependents: set[_Course]
 
     # extra / implement later
@@ -35,19 +48,28 @@ class _Course:
     corequisites: set[_Course]
 
     def __init__(self, code: str) -> None:
-        """Initialize a new course vertex with the given code, prerequisites, and dependents."""
+        """Initialize a new course vertex with the given code, prerequisites, and dependents.
+        """
+        super().__init__()
         self.code = code
-        self.prerequisites = []
+        self.prerequisites = BoolOp('and', [])
         self.dependents = set()
-        if code[6] == 'Y':
-            self.credit = 1.0
-        else:
-            self.credit = 0.5
+        self.credit = 1.0 if code[6] == 'Y' else 0.5
+        self.exclusions = set()
+        self.corequisites = set()
+
+    def evaluate(self) -> list:
+        """Evaluate the course to give itself and its prerequisites
+        """
+        return combine_lists3([{self.code}], self.prerequisites.evaluate())
+
+    def are_exclusions(self, course: _Course) -> bool:
+        """Check if self and course are exclusions of each other"""
+        return course in self.exclusions and self in course.exclusions
 
 
 class Graph:
-    """
-    A graph. Each vertice of the graph is a course.
+    """A graph. Each vertice of the graph is a course.
 
     Representation Invariants:
     """
@@ -61,14 +83,31 @@ class Graph:
         self._courses = {}
 
     def add_course(self, course_code: str) -> None:
-        """
-        Add a course to this graph.
+        """Add a course to this graph.
         The new course is not adjacent to any other vertices.
         """
         if course_code not in self._courses:
             self._courses[course_code] = _Course(course_code)
 
-    def add_prerequisites(self, prereq: set, course: str) -> None:
+    def add_exclusion(self, course_code: str, exclusion: set[str]) -> None:
+        """Add an exclusion to course code
+        """
+        if course_code in self._courses and all(exc in self._courses for exc in exclusion):
+            for ex in exclusion:
+                self._courses[course_code].exclusions.add(self._courses[ex])
+        else:
+            raise ValueError
+
+    def add_corequisites(self, course_code: str, coreq: set[str]) -> None:
+        """Add an exclusion to course code
+        """
+        if course_code in self._courses and all(cor in self._courses for cor in coreq):
+            for co in coreq:
+                self._courses[course_code].corequisites.add(self._courses[co])
+        else:
+            raise ValueError
+
+    def add_prerequisites2(self, prereq: set, course: str) -> None:
         """Updates a courses prerequisites. This funcitons takes in a set of related courses or "options"
         to meet a prerequisite requirement. The function will add one of these courses to all the current sets
         to create a list containing sets of all the different coursecombos needed to meet the prereq for that course.
@@ -77,27 +116,15 @@ class Graph:
         if (all({p in self._courses if isinstance(p, str) else all(p1 in self._courses for p1 in p) for p in prereq})
                 and course in self._courses):
             course_v = self._courses[course]
-            if course_v.prerequisites != []:
-                prereq_v = {self._courses[course_code] if isinstance(course_code, str)
-                            else tuple(self._courses[code] for code in course_code) for course_code in prereq}
-                combine_lists(course_v.prerequisites, prereq_v)
-            else:
-                for courses in prereq:
-                    if isinstance(courses, str):
-                        course_v.prerequisites.append({self._courses[courses]})
-                    else:
-                        course_v.prerequisites.append({self._courses[course_code] for course_code in courses})
+            prereq_v = [BoolOp('and', [self._courses[code] for code in course_code])
+                        if isinstance(course_code, tuple)
+                        else self._courses[course_code] for course_code in prereq]
+            course_v.prerequisites.operand.append(BoolOp('or', prereq_v))
         else:
             raise ValueError
 
     def add_dependents(self, course: str, dependent: str) -> None:
         """Add a dependant to a given course
-        >>> g = Graph()
-        >>> g.add_course('MAT135H1')
-        >>> g.add_course('MAT136H1')
-        >>> g.add_dependents('MAT135H1', 'MAT136H1')
-        >>> {c.code for c in g._courses['MAT135H1'].dependents}
-        {'MAT136H1'}
         """
         if course in self._courses and dependent in self._courses:
             course_v = self._courses[course]
@@ -106,22 +133,35 @@ class Graph:
         else:
             raise ValueError
 
-    def get_prerequisites(self, course_code: str) -> list:
+    def get_all_prerequisites(self, course_code: str) -> list:
         """Returns a list of courses that are prerequisites to the given course in str form
         """
+        prereqs = self._courses[course_code].prerequisites.evaluate()
+        for prereq_set in prereqs:
+            coreq = set()
+            for prereq in prereq_set:
+                coreqs = {co.code for co in self._courses[prereq].corequisites}
+                coreq.update(coreqs)
+            prereq_set.update(coreq)
 
-        if self._courses[course_code].prerequisites == []:
-            return []
-        else:
-            all_prereq = []
-            for course_set in self._courses[course_code].prerequisites:
-                new_prereq = []
-                for course in course_set:
-                    course_prereq = self.get_prerequisites(course.code).copy()
-                    combine_lists(course_prereq, {course.code})
-                    combine_lists2(new_prereq, course_prereq)
-                all_prereq += new_prereq
-            return all_prereq
+        return [set_prereq for set_prereq in prereqs if not any(
+            self._courses[pre].are_exclusions(self._courses[exclusion]) for pre in set_prereq for exclusion in
+            set_prereq)]
+
+    def get_prerequisites(self, course_code: str, completed: set[str], exclude: set[str], credit: float) -> list:
+        """Returns the pathways under the specified number of credits to meet the prerequisite for a given course
+        given a set of courses the user has already completed and a set of courses the user wants to avoid.
+        In the case of a tie, return all possibilities.
+        """
+        prereqs = self.get_all_prerequisites(course_code)
+        new_prereqs = [prereq_set for prereq_set in prereqs if not any(prereq in exclude for prereq in prereq_set)]
+        not_completed = [{prereq for prereq in prereq_set if prereq not in completed} for prereq_set in new_prereqs]
+        all_exclusions = {course_ex.code for completed_course in completed for course_ex in
+                          self._courses[completed_course].exclusions}
+        excluded = [prereq_set for prereq_set in not_completed if
+                    not any(prereq in all_exclusions for prereq in prereq_set)]
+        return [pathway for pathway in sorted([(count_credits(course_set), course_set) for course_set in excluded]) if
+                pathway[0] <= credit]
 
     def get_dependents(self, course_code: str) -> set:
         """Returns a list of courses that are dependents to the given course
@@ -130,65 +170,64 @@ class Graph:
         return {course.code for course in self._courses[course_code].dependents}
 
 
-def combine_lists(lst1: list[set], set2: set) -> None:
-    """Mutate list 1 such that each set in lst1 has exactly one elemnt of set2
-    for as many times as sets in lst2. Special mutation to account for posisble tuples in set2.
-    >>> lst1 = [{1, 2, 3}, {4}]
-    >>> combine_lists(lst1,{5, 6} )
-    >>> len(lst1)
-    4
-    >>> lst1 = []
-    >>> combine_lists(lst1,{5, 6} )
-    >>> lst1
-    [{5}, {6}]
+class BoolOp(Expr):
+    """and/or class
     """
-    if lst1 == []:
-        for set_element in set2:
-            lst1.append({set_element})
-    else:
-        new_lst = lst1[:]
-        for set_element in set2:
-            for list_element in new_lst:
-                if isinstance(set_element, tuple):
-                    lst1.append(list_element.union(set(set_element)))
-                else:
-                    lst1.append(list_element.union({set_element}))
-        for element in new_lst:
-            lst1.remove(element)
+    op: str
+    operand: list[Expr]
+
+    def __init__(self, operator: str, operands: list[Expr]) -> None:
+        super().__init__()
+        self.op = operator
+        self.operand = operands
+
+    def evaluate(self) -> list:
+        """This function returns a list of sets of different possibilities to meet an outcome. For example, if we have
+        the BoolOp tree 'and' with operands A, B, and BoolOp('or', [C, D]), then we get
+        [{A, B, C}, {A, B, D}]
+        """
+        if not self.operand:
+            return []
+        else:
+            if self.op == 'and':
+                new_list = []
+                for operand in self.operand:
+                    new_list = combine_lists3(new_list, [operand.evaluate()]) \
+                        if isinstance(operand.evaluate(), set) \
+                        else combine_lists3(new_list, operand.evaluate())
+            else:
+                new_list = []
+                for operand in self.operand:
+                    new_list = new_list + operand.evaluate() \
+                        if isinstance(operand.evaluate(), list) \
+                        else new_list + [operand.evaluate()]
+            return new_list
 
 
-def combine_lists2(lst1: list[set], lst2: list[set]) -> None:
+def combine_lists3(lst1: list[set], lst2: list[set]) -> list:
     """Mutate list 1 such that each set in lst1 has exactly one elemnt of list2
     for as many times as sets in lst2. Lst2 only has sets that have no tuples.
-    >>> lst1 = [{1, 2, 3}, {4}]
-    >>> combine_lists2(lst1, [{5, 6}, {7, 8}])
-    >>> len(lst1)
-    4
-    >>> lst1 = []
-    >>> combine_lists2(lst1, [{5, 6}, {7, 8}])
-    >>> lst1 == [{5, 6}, {7, 8}]
-    True
-    >>> lst1 = [{1, 2, 3}, {4}]
-    >>> combine_lists2(lst1, [{5}])
-    >>> len(lst1)
-    2
+    Preconditions:
+        - lst1 != [] or lst2 != []
     """
-    if lst1 == []:
-        for set_element in lst2:
-            lst1.append(set_element)
+    if not lst1:
+        return lst2
+    elif not lst2:
+        return lst1
     else:
-        new_lst = lst1[:]
-        for element in lst2:
-            for list_element in new_lst:
-                lst1.append(list_element.union(element))
-        for element in new_lst:
-            lst1.remove(element)
+        return [element1.union(element2) for element1 in lst1 for element2 in lst2]
+
+
+def count_credits(course_set: set[str]) -> int:
+    """Count the number of credits in a set of strings\
+    """
+    return sum([1.0 if course[6] == 'Y' else 0.5 for course in course_set])
 
 
 if __name__ == '__main__':
     import doctest
 
-    doctest.testmod()
+    doctest.testmod(verbose=True)
 
     import python_ta
 
