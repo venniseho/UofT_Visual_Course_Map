69801db542d21e571ab82dd80d8cc965d0f86594
Vennise Ho <venniseho14@gmail.com>
2024-03-31 16:54:18 -0400

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -23,6 +23,11 @@ def parse_row(row):
         prerequisites = prerequisites.replace(' ', '')
         prerequisites = clean_brackets(prerequisites, False)
         prerequisites = clean_brackets(prerequisites, True)
+        prerequisites = remove_duplicates(prerequisites, ',')
+        prerequisites = remove_duplicates(prerequisites, '/')
+        prerequisites = empty_brackets(prerequisites)
+
+        print(prerequisites, '--------', row['Prerequisites'])
 
     # else:
     #     print('NA', prerequisites)
@@ -66,7 +71,7 @@ def empty_brackets(s: str) -> str:
     while i < len(s):
         char = s[i]
         if char == '(':
-            r, num_char = empty_brackets_helper(s[i:])
+            r, num_char = empty_brackets_helper(s[i:], 0)
             new_s += r
             i += num_char
 
@@ -77,9 +82,9 @@ def empty_brackets(s: str) -> str:
     return new_s
 
 
-def empty_brackets_helper(s: str) -> tuple[str, int]:
+def empty_brackets_helper(s: str, bracket_depth: int) -> tuple[str, int]:
     if s == '()':
-        return ('', 1)
+        return ('', 2)
 
     new_s = ''
     i = 0
@@ -88,23 +93,37 @@ def empty_brackets_helper(s: str) -> tuple[str, int]:
         char = s[i]
 
         if char == '(':
-            r, skip = empty_brackets_helper(s[i + 1:])
+            r, skip = empty_brackets_helper(s[i + 1:], bracket_depth + 1)
             new_s += r
             i += skip
+
+            if bracket_depth == 0:
+                return new_s, i
             continue
 
         elif char == ')':
             if new_s != '':
                 return '(' + new_s + ')', i + 2
             else:
-                return '', i + 1
+                return '', i + 2
 
         new_s += char
         i += 1
     return new_s, i + 1
 
 
+def remove_duplicates(s: str, char: str):
+    return char.join([i for i in s.split(char) if i != ''])
+
+
 if __name__ == '__main__':
-    # parse('output.xlsx')
+    parse('output.xlsx')
     # print(empty_brackets('a((d)gh)(ij)()'))
-    print(empty_brackets('()'))
+    # print(empty_brackets('()'))
+    # print(empty_brackets('(a)(b)'))
+    # print(empty_brackets('(()())'))
+    # print(empty_brackets('((()())(a(d)))'))
+    # print(empty_brackets('()()(a,()b/(c()(d)),(e))'))
+    # print(',,,,,,'.replace(',,', ','))
+    # my_string = 'CSC110,,,,MAT137,,'
+    # print(','.join([x for x in my_string.split(',') if x != '']))
