f5100d08f27a0bf3f7c3fe19d9ddcfdd92395b91
Vennise Ho <venniseho14@gmail.com>
2024-04-04 11:11:11 -0400

    Update user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -10,6 +10,56 @@ Creators:
 - Vennise Ho
 """
 
+def commands() -> None:
+    """
+    Prints out a list of commands the user can use to interact with our interface.
+    """
+    commands = ['Get Prerequisites', 'Get Dependents', 'Display Course', 'Browse', 'Help']
+    print(f'LIST OF COMMANDS:'
+          f'- Get Prerequisites'
+          f'- Get Dependents'
+          f'- Display Course'
+          f'- Help')
+
+    print()
+
+    user_input = input('What would you like to do? ')
+
+    while user_input not in commands:
+        user_input = input('Invalid input. What would you like to do? ')
+
+    # action after selecting valid command
+
+    if user_input == 'help':
+        course_map_help()
+
+
+def course_map_help() -> None:
+    """
+    Tells the user what each command does.
+    """
+    user_input = input('What command would you like help with? ')
+
+    while user_input not in commands:
+        user_input = input('Invalid input. What would command would you like help with?? ')
+
+    if user_input == 'Get Prerequisites':
+        print(f'Gets the prerequisites needed for a given course. '
+              f'Once you type this command in, you will be prompted to provide a course.'
+              f'The algorithm will provide a list of courses required as prerequisites for the course you typed in and '
+              f'will also display this graph visually (a new window will pop up).')
+
+    elif user_input == ' Get Dependents':
+        print(f'Gets the dependents of a given course. '
+              f'Once you type this command in, you will be prompted to provide a course.'
+              f'The algorithm will provide a list of courses that require the course you typed in as a prerequisite and'
+              f'will also display this graph visually (a new window will pop up).')
+
+    elif user_input == 'Display Course':
+        print(f'Displays the given course visually, along with its prerequisites and dependents. '
+              f'Once you type this command in, you will be prompted to provide a course.'
+              f'The algorithm will display the graph of the course you typed in visually (a new window will pop up).')
+
 """
 What prerequisites are needed to take a particular course?
 """
